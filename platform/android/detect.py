660e3a89932d813825cba142a6b0e04f26d966e1
Dan <361560+Gromph@users.noreply.github.com>
2022-02-21 10:58:58 -0800

    Build release Android with debug symbols, the aab bundle will contain both stripped and unstripped .so libs This is to get call stacks of crashes in Google Play

--- platform/android/detect.py
+++ platform/android/detect.py
@@ -29,6 +29,7 @@ def get_opts():
         ("ndk_platform", 'Target platform (android-<api>, e.g. "android-19")', "android-19"),
         EnumVariable("android_arch", "Target architecture", "armv7", ("armv7", "arm64v8", "x86", "x86_64")),
         BoolVariable("android_neon", "Enable NEON support (armv7 only)", True),
+        BoolVariable("debug_symbols", "Add debugging symbols to release/release_debug builds", True),
     ]
 
 
@@ -198,6 +199,9 @@ def configure(env):
         elif env["optimize"] == "size":  # optimize for size
             env.Append(CCFLAGS=["-Os"])
             env.Append(LINKFLAGS=["-Os"])
+
+        if env["debug_symbols"]:
+            env.Prepend(CCFLAGS=["-g"])
 
         env.Append(CPPDEFINES=["NDEBUG"])
         if can_vectorize:
