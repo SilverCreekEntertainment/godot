71c0c743762641a95545f4d21240a9dfe16ca444
SCEManny <manny@silvercrk.com>
2023-01-26 12:02:57 -0800

    Update show_virtual_keyboard for tvOS to include our added paramters (currently ignored, but the keyboard shows now)

--- platform/tvos/os_tvos.mm
+++ platform/tvos/os_tvos.mm
@@ -110,7 +110,7 @@ bool OSAppleTV::has_virtual_keyboard() const {
 	return true;
 };
 
-void OSAppleTV::show_virtual_keyboard(const String &p_existing_text, const Rect2 &p_screen_rect, bool p_multiline, int p_max_input_length, int p_cursor_start, int p_cursor_end) {
+void OSAppleTV::show_virtual_keyboard(const String &p_existing_text, const Rect2 &p_screen_rect, bool p_multiline, int p_max_input_length, int p_cursor_start, int p_cursor_end, const String &p_input_type, const String &p_done_label) {
 	NSString *existingString = [[NSString alloc] initWithUTF8String:p_existing_text.utf8().get_data()];
 
 	[AppDelegate.viewController.keyboardView
