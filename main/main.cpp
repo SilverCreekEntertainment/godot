38406f006c5df76336f3a0b67c4c903beabcb0f1
dan <dan@silvercrk.com>
2024-10-29 12:24:16 -0700

    force more intel igpu's to use libangle to avoid blackbar at top of window problem

--- main/main.cpp
+++ main/main.cpp
@@ -2139,6 +2139,10 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 
 		// Added by SCE, works, but blackbar at top
 		// https://www.techpowerup.com/gpu-specs/   to find similar GPUs
+		BLOCK_DEVICE("Intel", "Intel(R) HD Graphics");  // user reported this model with no number, so this will match all models with this string in it
+		BLOCK_DEVICE("Intel", "Intel HD Graphics");
+		BLOCK_DEVICE("Intel", "Intel(R) Iris");
+		BLOCK_DEVICE("Intel", "Intel Iris");
 		BLOCK_DEVICE("Intel", "Intel HD Graphics 4400");
 		BLOCK_DEVICE("Intel", "Intel HD Graphics 4600");
 		BLOCK_DEVICE("Intel", "Intel HD Graphics 5300");
